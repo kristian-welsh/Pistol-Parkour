c8a7907e742f500cc58a5dccaca64f961709feac
Kristian Welsh <kriswelsh@gmail.com>
2019-01-22 11:09:46 +0000

    initial work on player motion

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float sensitivity = 5f;
+    public bool inverted = false;
+    Transform playerTransform;
+    int inv;
+
+    // Use this for initialization
+    void Start () {
+        playerTransform = transform.parent;
+        inv = (inverted ? 1 : -1);
+        Cursor.visible = false;
+    }
+	
+	void FixedUpdate () {
+        // todo: dont allow head to rotate fully up and backwards to break neck
+        Vector2 md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        md = Vector2.Scale(md, new Vector2(sensitivity, sensitivity));
+        md.y *= inv;
+
+        Quaternion yrot = Quaternion.AngleAxis(-md.y, Vector3.right);
+        Quaternion xrot = Quaternion.AngleAxis(md.x, playerTransform.up);
+        transform.Rotate(yrot.eulerAngles);
+        playerTransform.Rotate(xrot.eulerAngles);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    Vector3 movement = new Vector3();
+    Rigidbody playerRigidbody;
+
+    void Start () {
+        playerRigidbody = GetComponent<Rigidbody>();
+        
+	}
+
+    // called once per physics update
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        
+        Move(h, v);
+        Animate(h, v);
+    }
+
+    private void Move(float h, float v)
+    {
+        // todo: move relative to local z axis
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    private void Animate(float h, float v)
+    {
+        //throw new NotImplementedException();
+    }
+}
