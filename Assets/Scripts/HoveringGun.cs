cbf366a2b639b00ecf467e2a54653d5de863a70d
Kristian Welsh <kriswelsh@gmail.com>
2019-01-25 11:02:23 +0000

    wip collectable guns, scripting left to do

---
+++ Assets/Scripts/HoveringGun.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class HoveringGun : MonoBehaviour {
+    public float rotationSpeed = 2f;
+    public float bounceSpeed = 2f;
+    public float bounceMagnitude = 0.5f;
+
+    float rotationTime;
+    float bounceTime;
+    GameObject gun;
+    Vector3 startingPosition;
+
+    void Start () {
+        gun = transform.GetChild(0).gameObject;
+        startingPosition = gun.transform.position;
+    }
+    
+    void FixedUpdate()
+    {
+        // todo: detect collision with player
+    }
+
+    void Update()
+    {
+        rotationTime += Time.deltaTime;
+        rotationTime %= rotationSpeed;
+        float rotationAmount = (rotationTime / rotationSpeed) * 360;
+        transform.eulerAngles = new Vector3(0, rotationAmount, 0);
+
+        // deltabounce is nessecary because the gun's origin isn't centere on the object
+        // meaning setting its position relative to start position nullifies change in
+        // position from parent's rotation
+        bounceTime += Time.deltaTime;
+        bounceTime %= bounceSpeed;
+        float bounceAmount = Mathf.Sin((bounceTime / bounceSpeed) * 2 * Mathf.PI) * bounceMagnitude;
+        //float deltaBounce = 
+        gun.transform.position = gun.transform.position + new Vector3(0, bounceAmount * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+    public float range = 100f;
+    
+    float timer;
+    Ray shootRay = new Ray();
+    RaycastHit shootHit;
+    int shootableMask;
+    //ParticleSystem gunParticles;
+    LineRenderer gunLine;
+    AudioSource gunAudio;
+    Light gunLight;
+    float effectsDisplayTime = 0.2f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        shootableMask = LayerMask.GetMask("Shootable");
+        //gunParticles = GetComponent<ParticleSystem>();
+        gunLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        gunLight = GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+        {
+            timer = 0f;
+            Shoot();
+        }
+
+        if (timer >= timeBetweenBullets * effectsDisplayTime)
+        {
+            DisableEffects();
+        }
+    }
+
+    private void DisableEffects()
+    {
+        gunLine.enabled = false;
+        gunLight.enabled = false;
+    }
+
+    private void Shoot()
+    {
+        gunAudio.Play();
+
+        gunLight.enabled = true;
+
+        //gunParticles.Stop();
+        //gunParticles.Play();
+
+        gunLine.enabled = true;
+        gunLine.SetPosition(0, transform.position);
+
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+        {
+            /*
+            Health enemyHealth = shootHit.collider.GetComponent<Health>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damagePerShot, shootHit.point);
+            }
+            */
+            gunLine.SetPosition(1, shootHit.point);
+        }
+        else
+        {
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+}
