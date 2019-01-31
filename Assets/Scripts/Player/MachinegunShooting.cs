b10d0995b5c7d040c2ae80d9d1633a737d2a2468
Kristian Welsh <kjw28@sussex.ac.uk>
2019-01-31 14:27:54 +0000

    wip shotgun implementation

---
+++ Assets/Scripts/Player/MachinegunShooting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachinegunShooting : PlayerShooting
+{
+    public float range = 100f;
+    
+    int shootableMask;
+    LineRenderer gunLine;
+    Light gunLight;
+
+    void Start()
+    {
+        base.Start();
+        shootableMask = LayerMask.GetMask("Shootable");
+        gunLine = GetComponent<LineRenderer>();
+        gunLight = GetComponent<Light>();
+    }
+
+    protected override void DisableEffects()
+    {
+        gunLine.enabled = false;
+        gunLight.enabled = false;
+    }
+
+    protected override void Shoot()
+    {
+        gunAudio.Play();
+        gunLight.enabled = true;
+        gunLine.enabled = true;
+        gunLine.SetPosition(0, transform.position);
+
+        Ray shootRay = new Ray(transform.position, transform.forward);
+        RaycastHit shootHit;
+        if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+        {
+            gunLine.SetPosition(1, shootHit.point);
+        }
+        else
+        {
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+    
+    float timer;
+    float effectsDisplayTime = 0.2f;
+
+    protected AudioSource gunAudio;
+
+    protected void Start ()
+    {
+        gunAudio = GetComponent<AudioSource>();
+    }
+	
+	void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+        {
+            timer = 0f;
+            Shoot();
+        }
+
+        if (timer >= timeBetweenBullets * effectsDisplayTime)
+        {
+            DisableEffects();
+        }
+    }
+
+    protected virtual void DisableEffects() { }
+
+    protected virtual void Shoot() { }
+}
