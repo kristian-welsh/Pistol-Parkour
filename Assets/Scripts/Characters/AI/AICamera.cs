fc69ea4ad55d1fd82f256460bbb3a0e93e7b6183
Kristian Welsh <kriswelsh@gmail.com>
2019-02-12 15:10:02 +0000

    refactoring AICamera

--- Assets/Scripts/Characters/AI/AICamera.cs
+++ Assets/Scripts/Characters/AI/AICamera.cs
@@ -4,8 +4,8 @@ public class AICamera : CharacterCamera
 {
 	public GameObject player;
 
-	private Vector3 charRot = Vector3.zero;
-	private Vector3 camRot = Vector3.zero;
+	private Vector3 characterRotation = Vector3.zero;
+	private Vector3 cameraRotation = Vector3.zero;
 
 	public override void Start ()
 	{
@@ -15,22 +15,33 @@ public class AICamera : CharacterCamera
 
 	public override void FixedUpdate ()
 	{
+		Vector3 facingDirection = TargetXZFacingDirection();
+		characterRotation = Vector3.up * YRotationNeeded(facingDirection);
+		base.FixedUpdate();
+	}
+
+	private Vector3 TargetXZFacingDirection()
+	{
 		Vector3 difference = player.transform.position - transform.position;
 		difference.y = 0;
-		float amount = Vector3.Angle(Vector3.forward, difference.normalized);
-		if (difference.x < 0)
+		return difference.normalized;
+	}
+
+	private float YRotationNeeded(Vector3 direction)
+	{
+		float amount = Vector3.Angle(Vector3.forward, direction);
+		if (direction.x < 0)
 			amount = 360 - amount;
-		charRot = Vector3.up * amount;
-		base.FixedUpdate();
+		return amount;
 	}
 	
 	protected override Quaternion NewCharRotation()
 	{
-		return Quaternion.Euler(charRot);
+		return Quaternion.Euler(characterRotation);
 	}
 
 	protected override Quaternion NewCameraRotation()
 	{
-		return Quaternion.Euler(camRot);
+		return Quaternion.Euler(cameraRotation);
 	}
 }
