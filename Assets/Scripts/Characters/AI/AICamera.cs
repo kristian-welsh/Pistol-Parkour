b3dd19b279f973e49e48d7013fcb3f46768019c7
Kristian Welsh <kjw28@sussex.ac.uk>
2019-02-15 12:06:42 +0000

    refactoring AICamera

--- Assets/Scripts/Characters/AI/AICamera.cs
+++ Assets/Scripts/Characters/AI/AICamera.cs
@@ -17,34 +17,34 @@ public class AICamera : CharacterCamera
 
 	public override void FixedUpdate ()
 	{
-		Vector3 facingDirection = TargetXZFacingDirection();
-		characterRotation = Vector3.up * YRotationNeeded(facingDirection);
-
-		//cameraRotation
-		Vector3 toPlayer = enemyMidpoint.position - transform.position;
-		Vector3 toPlayer2d = enemyMidpoint.position - transform.position;
-		toPlayer2d.y = 0;
-		float amount = Vector3.Angle(toPlayer2d, toPlayer);
-		if(toPlayer.y > 0)
-			amount = 360 - amount;
-		cameraRotation = new Vector3(amount, 0f, 0f);
-
+		characterRotation = Vector3.up * RotationNeeded(Vector3.forward, Direction2D(), -Direction2D().x);
+		cameraRotation = Vector3.right * RotationNeeded(Direction2D(), Direction(), Direction().y);
 		base.FixedUpdate();
 	}
 
-	private Vector3 TargetXZFacingDirection()
+	/* Returns the Euler rotation that needs to be performed on direction2d to reach direction3d
+	 * this rotation needs to be performed in the axis direction2d cross direction3d
+	 * currentDisplacement: current displacement on the axis that differentiates the two vectors,
+	 * this is nessecary to differentiate between 270 degrees and 90 degrees from the Vector3.Angle result.
+	 */
+	private float RotationNeeded(Vector3 direction2d, Vector3 direction3d, float currentDisplacement)
 	{
-		Vector3 difference = player.transform.position - transform.position;
-		difference.y = 0;
-		return difference.normalized;
-	}
-
-	private float YRotationNeeded(Vector3 direction)
-	{
-		float amount = Vector3.Angle(Vector3.forward, direction);
-		if (direction.x < 0)
+		float amount = Vector3.Angle(direction2d, direction3d);
+		if(currentDisplacement > 0)
 			amount = 360 - amount;
 		return amount;
+	}
+
+	private Vector3 Direction2D()
+	{
+		Vector3 direction = Direction();
+		direction.y = 0;
+		return direction;
+	}
+
+	private Vector3 Direction()
+	{
+		return enemyMidpoint.position - transform.position;
 	}
 	
 	protected override Quaternion NewCharRotation()
