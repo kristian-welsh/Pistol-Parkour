ad69eedcd6413aa0925e284e567fda876b0adfb1
Kristian Welsh <kjw28@sussex.ac.uk>
2019-02-16 12:19:52 +0000

    implement health on raycast guns

---
+++ Assets/Scripts/Characters/FPSHealth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FPSHealth : MonoBehaviour
+{
+	public int startingHealth = 100;
+
+	private int health;
+
+	void Start ()
+	{
+		Reset();
+	}
+
+	public void Reset()
+	{
+		health = startingHealth;
+	}
+
+	public void Damage(int amount)
+	{
+		health -= amount;
+		if(health < 0)
+		{
+			health = 0;
+			// todo: trigger death animations etc here
+		}
+	}
+}
--- Assets/Scripts/Guns/Shooting/RaycastGun.cs
+++ Assets/Scripts/Guns/Shooting/RaycastGun.cs
@@ -13,7 +13,7 @@ public class RaycastGun : GunShooting
         base.Start();
         gunLine = GetComponent<LineRenderer>();
         gunLight = GetComponent<Light>();
-		raycaster = new Raycaster(range, "Shootable");
+		raycaster = new Raycaster(range);
 
 	}
 
@@ -25,14 +25,33 @@ public class RaycastGun : GunShooting
 
     protected override void Shoot()
     {
+        DrawEffects();
+        DamageObjects();
+	}
+
+    private void DrawEffects()
+    {
         gunAudio.Play();
         gunLight.enabled = true;
         gunLine.enabled = true;
         gunLine.SetPosition(0, transform.position);
-		
-		RaycastHit? hit = raycaster.CastRay(transform.position, transform.forward);
-		Vector3 farEndPoint = transform.position + transform.forward * range;
-		Vector3 endPoint = hit.HasValue ? hit.Value.point : farEndPoint;
-		gunLine.SetPosition(1, endPoint);
-	}
+        
+        RaycastHit? hit = raycaster.CastRay(transform.position, transform.forward);
+        Vector3 farEndPoint = transform.position + transform.forward * range;
+        Vector3 endPoint = hit.HasValue ? hit.Value.point : farEndPoint;
+        gunLine.SetPosition(1, endPoint);
+    }
+
+    private void DamageObjects()
+    {
+        RaycastHit? hit = raycaster.CastRay(transform.position, transform.forward);
+        if(hit.HasValue)
+        {
+            GameObject objectHit = hit.Value.transform.gameObject;
+            print(objectHit.name);
+            FPSHealth health = objectHit.GetComponentInParent<FPSHealth>();
+            if(health != null)
+                health.Damage(damagePerShot);
+        }
+    }
 }
